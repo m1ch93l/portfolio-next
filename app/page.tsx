285ae11f3ca93c1f67d4413e22a39909e0d2e9e5
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-10-29 22:52:09 +0800

    Install and add social icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { SocialIcon } from 'react-social-icons'
+
 export default function Page() {
   return (
     <div className="header">Hello, I'm Michael Bañaria
       <div className="subheader">Your Future Innovator</div>
+      
+      <SocialIcon url="www.facebook.com" />
+      <SocialIcon url="www.twitter.com" />
+      <SocialIcon url="www.instagram.com" />
+      <SocialIcon url="www.github.com/m1ch93l" />
     </div>
   )
 }
