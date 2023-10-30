d8019590a010db61da59cef51acf92c24a5e92c6
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2023-10-30 23:18:52 +0800

    Add links to social icons

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Page() {
     <div className="header">Hello, I'm Michael Bañaria
       <div className="subheader">Your Future Innovator</div>
       
-      <SocialIcon url="www.facebook.com" />
-      <SocialIcon url="www.twitter.com" />
-      <SocialIcon url="www.instagram.com" />
-      <SocialIcon url="www.github.com/m1ch93l" />
+      <SocialIcon href='https://web.facebook.com/michael.banaria25/' url="www.facebook.com" />
+      <SocialIcon href='https://twitter.com/michaelbbanaria' url="www.twitter.com" />
+      <SocialIcon href='https://www.instagram.com/michael_25.97/' url="www.instagram.com" />
+      <SocialIcon href='https://github.com/m1ch93l' url="www.github.com/m1ch93l" />
     </div>
   )
 }
